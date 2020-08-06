f369f4de6a61a90013cb3bdf84de0d38129aea23
unknown <amiraj.1995@yahoo.com>
2020-08-06 14:12:50 -0700

    Debuging some issues

--- main.py
+++ main.py
@@ -1,118 +1,73 @@
-import sys
-import os
-
 # Importing necessary libraries
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-path = os.getcwd()+'\\pyEDA\\'
-sys.path.insert(0, path)
 # Importing necessary functions for feature extraction
-from openShimmerFile import *
-from preprocessing import *
-from filtering import *
-from pyEDA import *
-from DNN_Features import *
+from pyEDA.openShimmerFile import *
+from pyEDA.preprocessing import *
+from pyEDA.filtering import *
+from pyEDA.pyEDA import *
+from pyEDA.DNN_Features import *
 
-# You must edit these based on your files
-fileName = ['data1.dat', 'data2.csv' , 'data3.dat', 'data4.dat', 'data5.csv']
-columnName = 'microSiemens'
-X = [] # Input X for classifier
-Y = [] # Target Y for input X
-
-for i in fileName:
-  Raw_GSR = openShimmerFile(i, columnName)
-  gsrdata = np.array(Raw_GSR)
-  
-  plt.figure(figsize=(12,4))
-  plt.plot(gsrdata)
-  plt.show()
-
-  ############################################################################
-  ############################ Preprocessing Part ############################
-  
-  # Select the new sample rate, and windowing size
-  sample_rate=40
-  segment_width=600
-
-  # Resample the data based on original data rate of your device, here: 128Hz
-  data = resample_data(gsrdata, 128, sample_rate)
-
-  # Segmentwise the data based on window sizes
-  s_working_data, s_measures, gsrdata_segmentwise = segmentwise(data, sample_rate=sample_rate, segment_width=segment_width, segment_overlap=0)
-  
-  preprocessed_gsr = []
-  for i in gsrdata_segmentwise:
-    preprocessed_gsr.append(normalization(rolling_mean(i, 1./sample_rate, sample_rate)))
+def process(gsr_signal, sample_rate=128, new_sample_rate=40, segment_width=600, segment_overlap=0):
+	gsrdata = np.array(gsr_signal)
 	
-  ############################ Preprocessing Part ############################
-  ############################################################################
-  
-  
-  
-  #################################################################################
-  ############################ Feature Extraction Part ############################
-
-  # Statistical Feature Extraction
-  for i in preprocessed_gsr:
-    working_data, measures = statistical_feature_extraction(i, sample_rate)
-    for k in measures.keys():
-        s_measures = append_dict(s_measures, k, measures[k])
-    for k in working_data.keys():
-        s_working_data = append_dict(s_working_data, k, working_data[k])
-
-  wd = s_working_data
-  m = s_measures
-  
-  
-  # Deep Learning Feature Extraction
-  ###
-  
-  ##################################
-  # Just Need to load the trainde model here and use it for prediction
-  
-  model = create_1Dcnn()
-  path_to_DL_weights = './pyEDA/eda_deep_model.h5'
-  model.load_weights(path_to_DL_weights)
-
-  # Features extracted from Deep learning model
-  getFeature = deepFeatures(model)
-
-  # Fully Connected Network which can be used for the prediction
-  getPrediction = deepPrediction(model)
-  ##################################
-  ###
-  
-  ############################ Feature Extraction Part ############################
-  #################################################################################
-  
-  
-  
-  
-  ##########################################################################################
-  ############################ Visualizing Statistical Features ############################
-  
-  # Mapping the indexlist of each window to original data: comment it if you do not use windowing
-  for index,i2 in enumerate(wd['indexlist']):
-    for index2,j2 in enumerate(i2):
-      wd['indexlist'][index][index2] = j2+segment_width*sample_rate*index
-
-  # Storing the peaks for visualization
-  peaks = []
-  for index,i2 in enumerate(wd['indexlist']):
-    for index2,j2 in enumerate(i2):
-      peaks.append(j2)
-
-  # Visualize the data with detected peaks marked with "x"
-  ng = normalization(rolling_mean(data, 1./sample_rate, sample_rate))
-  plt.plot(ng)
-  plt.plot(peaks, ng[peaks], "x")
-  plt.show()
-
-  print(m['mean_gsr'])
-  print(m['number_of_peaks'])
-  print(m['max_of_peaks'])
-  
-  ############################ Visualizing Statistical Features ############################
-  ##########################################################################################
+	plt.figure(figsize=(12,4))
+	plt.plot(gsrdata)
+	plt.show()
+	
+	print("If you are using this tool for your research please cite this paper: \"GSR Analysis for Stress: Development and Validation of an Open Source Tool for Noisy Naturalistic GSR Data\"");
+	
+	############################################################################
+	############################ Preprocessing Part ############################
+	
+	# Select the new sample rate, and windowing size
+	new_sample_rate = new_sample_rate
+	segment_width = segment_width
+	
+	# Resample the data based on original data rate of your device, here: 128Hz
+	data = resample_data(gsrdata, sample_rate, new_sample_rate)
+	
+	# Segmentwise the data based on window sizes
+	s_working_data, s_measures, gsrdata_segmentwise = segmentwise(data, sample_rate=new_sample_rate, segment_width=segment_width, segment_overlap=segment_overlap)
+	
+	preprocessed_gsr = []
+	for i in gsrdata_segmentwise:
+		preprocessed_gsr.append(normalization(rolling_mean(i, 1./new_sample_rate, new_sample_rate)))
+	
+	############################ Preprocessing Part ############################
+	############################################################################
+	
+	
+	
+	#################################################################################
+	############################ Feature Extraction Part ############################
+	
+	# Statistical Feature Extraction
+	for i in preprocessed_gsr:
+		working_data, measures = statistical_feature_extraction(i, new_sample_rate)
+		for k in measures.keys():
+			s_measures = append_dict(s_measures, k, measures[k])
+		for k in working_data.keys():
+			s_working_data = append_dict(s_working_data, k, working_data[k])
+	
+	wd = s_working_data
+	m = s_measures
+	
+	"""
+	# Deep Learning Feature Extraction
+	# Just Need to load the trained model here and use it for prediction: 
+	# The current model is trained for stress detection feature extraction (WESAD dataset)
+	for i in preprocessed_gsr:
+		model = create_1Dcnn(len(i))
+		path_to_DL_weights = './pyEDA/eda_deep_model.h5'
+		model.load_weights(path_to_DL_weights)
+		
+		# Features extracted from Deep learning model
+		getFeature = deepFeatures(model)
+		
+		# Fully Connected Network which can be used for the prediction
+		getPrediction = deepPrediction(model)"""
+	
+	return m, wd
--- pyEDA/DNN_Features.py
+++ pyEDA/DNN_Features.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
 from tensorflow.keras.models import Model, load_model
@@ -8,7 +9,7 @@ from tensorflow.keras.optimizers import SGD, Adam
 import tensorflow.keras.backend as K
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
-def create_1Dcnn(n_filters=16):
+def create_1Dcnn(length, n_filters=16):
     METRICS = [
           keras.metrics.TruePositives(name='tp'),
           keras.metrics.FalsePositives(name='fp'),
@@ -20,7 +21,7 @@ def create_1Dcnn(n_filters=16):
           keras.metrics.AUC(name='auc'),
     ]
     model = models.Sequential()
-    model.add(layers.Conv1D(filters=n_filters, kernel_size=3, activation='relu', input_shape=(2460,1)))
+    model.add(layers.Conv1D(filters=n_filters, kernel_size=3, activation='relu', input_shape=(length,1)))
     model.add(layers.BatchNormalization())
     # model.add(layers.Conv1D(filters=n_filters*2, kernel_size=3, activation='relu'))
     # model.add(layers.BatchNormalization())
